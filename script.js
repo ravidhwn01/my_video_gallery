d81da7503c55f1ba6a848196aa9d521699849de7
ravidhwn01 <87488989+ravidhwn01@users.noreply.github.com>
2022-06-22 08:25:00 +0530

    video gallery

---
+++ script.js
@@ -0,0 +1,14 @@
+let video = document.querySelectorAll('video');
+video.forEach(play =>
+  play.addEventListener('click ', () => {
+    
+    play.classList.toggle('active')
+    if(play.paused){
+        play.play();
+    }
+    else{
+        play.pause();
+       play.currentTime = 0;
+    }
+  })
+);
